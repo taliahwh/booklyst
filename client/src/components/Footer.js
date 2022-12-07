69875401cd7a85de621cce86b941f910ca0c3ddb
Taliah Wharton <taliahwh23@yahoo.com>
2022-12-07 14:58:01 -0500

    linked github

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,11 @@ const Footer = () => {
   return (
     <footer>
       <hr className="border-gray-200  " />
-      <p className="text-center text-gray-400 text-sm py-3">
-        Copyright &copy; booklyst
+      <p
+        href="https://github.com/taliahwh/booklyst"
+        className="text-center text-gray-400 text-sm py-3"
+      >
+        github
       </p>
     </footer>
   );
