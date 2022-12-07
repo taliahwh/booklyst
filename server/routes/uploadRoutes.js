c2551b3349bccf6a0be34a63e831bd9c215d05c0
Taliah Wharton <taliahwh23@yahoo.com>
2022-12-07 14:58:31 -0500

    multer for image uploading

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -5,9 +5,6 @@ import multer from 'multer';
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
   filename(req, file, cb) {
     cb(
       null,
